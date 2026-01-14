6ac8184a3b9e7bc61dd48a8c81efd24ad2f8c627
Marc von Renteln <marc@von-renteln.de>
2026-01-14 15:19:31 +0100

    fix(Test): path of vitest files

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: "../vitest.setup.ts",
+    setupFiles: "./vitest.setup.ts",
     css: true,
   },
 });
