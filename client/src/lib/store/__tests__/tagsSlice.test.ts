bb2267f9ea305ab272d97f0ae69fbafcd587f331
Marc von Renteln <marc@von-renteln.de>
2026-01-14 13:09:40 +0100

    revert: use alias import for test helper

--- client/src/lib/store/__tests__/tagsSlice.test.ts
+++ client/src/lib/store/__tests__/tagsSlice.test.ts
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, it } from "vitest";
 import { useTranscriptStore } from "@/lib/store";
-import { createTestSegment } from "../../../test/testSegmentHelper";
+import { createTestSegment } from "@/test/testSegmentHelper";
 
 describe("tagsSlice", () => {
   beforeEach(() => {
