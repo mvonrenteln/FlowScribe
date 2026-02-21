84e832e75e7365fa8260c1f75a02b364c1f4e9cb
Marc von Renteln <marc@von-renteln.de>
2026-02-21 17:43:38 +0100

    Remove wouter router wrapper for single-page app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,9 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Route, Switch } from "wouter";
 import { I18nProvider } from "@/components/i18n/I18nProvider";
 import { TranscriptEditor } from "@/components/TranscriptEditor";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
 import { queryClient } from "./lib/queryClient";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={TranscriptEditor} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 export function App() {
   return (
@@ -22,7 +11,7 @@ export function App() {
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <TranscriptEditor />
         </TooltipProvider>
       </QueryClientProvider>
     </I18nProvider>
